efef179af9736aabd0918352cdc76036d44dcf21
Racle <43697210+RacleRay@users.noreply.github.com>
2023-09-04 14:27:51 +0800

    Update myshell.c
    update myshell.c, more readable

--- myshell/myshell.c
+++ myshell/myshell.c
@@ -10,77 +10,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+
 #define MAXPIPES 20
+#define PIPEIN  1
+#define PIPEOUT 0
 
-char* strip(char* str) {
-    int head = 0, tail = strlen(str) - 1;
-    while (isspace(str[head])) {
-        head++;
-    }
-    while (isspace(str[tail])) {
-        str[tail--] = '\0';
-    }
-    return str + head;
-}
+#define MAX_ARGV 100
 
-void do_work(char* buf) {
-    // redirect
-    int tarfd = -1;
-    if (strstr(buf, "<")) {
-        tarfd = 0;
-    }
-    else if (strstr(buf, ">")) {
-        tarfd = 1;
-    }
+#define REDIRECT_FILE_MASK 0664
 
-    char* cmd  = NULL;
-    char* file = NULL;
-    if (tarfd == -1) {
-        cmd = strip(buf);
-    }
-    else {
-        cmd  = strip(strtok(buf, "><"));
-        file = strip(strtok(NULL, "><"));
 
-        // redirect
-        int fd;
-        if (tarfd == 0) {
-            fd = open(file, O_RDONLY);
-        }
-        else {
-            fd = open(file, O_RDWR | O_CREAT | O_TRUNC, 0664);
-        }
-        close(tarfd);
-        dup2(fd, tarfd);
-        close(fd);
-    }
+char* strtrim(char* str);
+void do_work(char *buf);
 
-    printf("cmd=[%s], file=[%s]\n", cmd, file);
-
-    char* argv[100];
-    int   i = 0;
-
-    argv[i++] = strtok(cmd, " ");
-    printf("%s\n", argv[i - 1]);
-    while ((argv[i++] = strtok(NULL, " "))) {
-        printf("%s\n", argv[i - 1]);
-    }
-    if (execvp(cmd, argv) < 0) {
-        perror("execvp");
-        exit(1);
-    }
-
-    return;
-}
 
 int main() {
     char buf[1024] = {0};
 
+    // signal background write to shell
     signal(SIGTTOU, SIG_IGN);
 
     while (1) {
@@ -89,91 +42,155 @@ int main() {
         printf("\033[32muser\033[37m@\033[31mname\033[0m $");
         scanf("%[^\n]s", buf);
         getchar();
-        if (!strcmp(buf, "exit")) {
+
+        if (!strcmp(buf, "exit")) {  // exit input
             break;
-        }
-        else if (!buf[0]) {
+        } else if (!buf[0]) {  // empty input
             continue;
         }
 
-        int   i = 0;
+        // read the commands piped.
+        int n_cmd = 0;
         char* cmdarr[MAXPIPES];
-        cmdarr[i++] = strtok(buf, "|");
-        while ((cmdarr[i] = strtok(NULL, "|"))) {
-            i++;
+        cmdarr[n_cmd++] = strtok(buf, "|");
+        while ((cmdarr[n_cmd] = strtok(NULL, "|"))) {
+            n_cmd++;
         }
 
         pid_t pid, pgrp;
-        // no pipe
+        // no pipe input
         if (!cmdarr[1]) {
             if ((pid = fork()) < 0) {
                 perror("fork");
                 exit(1);
             }
-            if (pid == 0) {
+            if (pid == 0) { // child
                 do_work(buf);
                 exit(0);
-            }
-            else {
+            } else {       // parent
                 wait(NULL);
                 continue;
             }
-        }
-        else {
-            // printf("WITH PIPE");
-            int pipefd[MAXPIPES][2];
-            int j;
-            for (j = 0; j < i - 1; ++j) {
-                if (pipe(pipefd[j]) < 0) {
-                    perror("pipe");
-                    exit(1);
+        } else { // with piped commands
+            int pipefd[MAXPIPES][2];   // pipe for data stream
+            for (int i = 0; i < n_cmd - 1; ++i) {
+                // write to pipefd[1], read from pipefd[0]
+                if (pipe(pipefd[i]) < 0) {
+                    perror("pipe failed");
+                    exit(-1);
                 }
             }
 
-            for (j = 0; j < i; ++j) {
+            // run cmd
+            for (int i = 0; i < n_cmd; ++i) {
                 if ((pid = fork()) < 0) {
-                    perror("fork");
-                    exit(1);
+                    perror("fork failed");
+                    exit(-2);
                 }
                 if (pid == 0) {
-                    if (j == 0) {
-                        dup2(pipefd[j][1], 1);
+                    if (i == 0) {
+                        // get data from parent
+                        dup2(pipefd[i][PIPEIN], STDOUT_FILENO);  // curren out to first pipe in
+                    } else if (i == n_cmd - 1)  {
+                        dup2(pipefd[i - 1][PIPEIN], STDIN_FILENO);  // last pipefd out to stdin
+                    } else {
+                        // current stdfd connect to pipe before and pipe after
+                        dup2(pipefd[i - 1][PIPEOUT], STDIN_FILENO);  // last pipe out to current stdin
+                        dup2(pipefd[i][PIPEIN], STDOUT_FILENO);      // current stdin to next pipe in
                     }
-                    else if (j == i - 1) {
-                        dup2(pipefd[j - 1][0], 0);
+
+                    do_work(cmdarr[i]);
+
+                } else {
+                    if (i == 0) {
+                        pgrp = pid;  // first child id is group id
                     }
-                    else {
-                        dup2(pipefd[j - 1][0], 0);
-                        dup2(pipefd[j][1], 1);
-                    }
-                    for (int k = 0; k < i - 1; ++k) {
-                        close(pipefd[k][0]);
-                        close(pipefd[k][1]);
-                    }
-                    do_work(cmdarr[j]);
-                }
-                else {
-                    if (j == 0) {
-                        pgrp = pid;
-                    }
-                    setpgid(pid, pgrp);
-                    //控制信号，显示前台进程组id
-                    tcsetpgrp(0, pgrp);
+                    setpgid(pid, pgrp);  // set a group of process
+                    tcsetpgrp(0, pgrp);  // set foreground group id
                 }
             }
 
-            for (int k = 0; k < i - 1; ++k) {
-                close(pipefd[k][0]);
-                close(pipefd[k][1]);
+            for (int j = 0; j < n_cmd - 1; ++j) {
+                close(pipefd[j][PIPEOUT]);
+                close(pipefd[j][PIPEOUT]);
             }
 
-            for (int k = 0; k < i; ++k) {
+            for (int j = 0; j < n_cmd; ++j) {
                 wait(NULL);
             }
 
-            tcsetpgrp(0, getpid());
+            // back to parent foreground process group
+            tcsetpgrp(0, getpgid(getpid()));
         }
     }
 
     return 0;
+}
+
+
+void do_work(char* buf) {
+    int tarfd = -1;
+    if (strstr(buf, "<")) {
+        tarfd = STDIN_FILENO;
+    } else if (strstr(buf, ">")) {
+        tarfd = STDOUT_FILENO;
+    }
+
+    char* cmd = NULL;
+    char* file = NULL;
+
+    // without redirection
+    if (tarfd == -1) {
+        cmd = strtrim(buf);
+    } else {
+        cmd = strtrim(strtok(buf, "<>"));
+        file = strtrim(strtok(NULL, "<>"));
+
+        int fd;
+        if (tarfd == STDIN_FILENO) {
+            fd = open(file, O_RDONLY);
+        } else {
+            fd = open(file, O_RDWR | O_CREAT | O_TRUNC, REDIRECT_FILE_MASK);
+        }
+
+        close(tarfd);
+        dup2(fd, tarfd);
+        close(fd);
+    }
+
+#ifdef _DEBUG
+    printf("cmd=[%s], file=[%s]\n", cmd, file);
+#endif
+
+    char* argv[MAX_ARGV];
+    int i = 0;
+    argv[i++] = strtok(cmd, " ");
+#ifdef _DEBUG
+    printf("%s ", argv[i - 1]);
+#endif
+
+    while ((argv[i++] = strtok(NULL, " "))) {
+        printf("%s ", argv[i - 1]);
+    }
+
+    if (execvp(cmd, argv) < 0) {
+        perror("execvp");
+        exit(-3);
+    }
+
+    return;
+}
+
+
+char* strtrim(char *str) {
+    int head = 0, tail = strlen(str) - 1;
+
+    while (isspace(str[head])) {
+        head++;
+    }
+    while (isspace(str[tail])) {
+        str[tail--] = '\0';
+    }
+
+    return str + head;
 }
